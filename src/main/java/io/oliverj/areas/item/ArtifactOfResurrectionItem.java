8786ddc6f240d86b312f802f16edf703063a7e23
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-05-06 09:24:44 -0400

    PlayerDeathMixin now injects into PlayerEntity

--- src/main/java/io/oliverj/areas/item/ArtifactOfResurrectionItem.java
+++ src/main/java/io/oliverj/areas/item/ArtifactOfResurrectionItem.java
@@ -3,13 +3,20 @@ package io.oliverj.areas.item;
 import com.google.common.collect.Multimap;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
+import io.oliverj.areas.registry.ArtifactRegister;
 import io.oliverj.areas.registry.CardinalComponentRegister;
+import io.wispforest.owo.nbt.NbtKey;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtInt;
+import net.minecraft.nbt.NbtTypes;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -30,7 +37,14 @@ public class ArtifactOfResurrectionItem extends TrinketItem {
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         if (stack.getHolder() instanceof PlayerEntity) {
-            tooltip.add(Text.literal("Uses Left: " + stack.getHolder().getComponent(CardinalComponentRegister.RESURRECTION).getUses()));
+            tooltip.add(Text.literal("Uses Left: " + stack.getNbt().get("uses").toString()));
         }
+    }
+
+    @Override
+    public ItemStack getDefaultStack() {
+        ItemStack itemStack = new ItemStack(ArtifactRegister.RESURRECT_ARTIFACT);
+        itemStack.setSubNbt("uses", NbtInt.of(10));
+        return itemStack;
     }
 }
--- src/main/java/io/oliverj/areas/mixin/PlayerDeathMixin.java
+++ src/main/java/io/oliverj/areas/mixin/PlayerDeathMixin.java
@@ -1,30 +1,45 @@
 package io.oliverj.areas.mixin;
 
+import com.mojang.logging.LogUtils;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
+import dev.onyxstudios.cca.api.v3.component.Component;
 import io.oliverj.areas.Areas;
+import io.oliverj.areas.item.ArtifactOfResurrectionItem;
 import io.oliverj.areas.registry.ArtifactRegister;
 import io.oliverj.areas.registry.CardinalComponentRegister;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtInt;
 import net.minecraft.text.Text;
+import net.minecraft.util.Pair;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
+import java.util.List;
+
+@Mixin(PlayerEntity.class)
 public class PlayerDeathMixin {
     @Inject(method = "onDeath", at = @At(value = "HEAD"), cancellable = true)
     private void onDeath(DamageSource damageSource, CallbackInfo ci) {
-        ci.cancel();
+        LogUtils.getLogger().info("OnDeath: AREAS");
         Areas.LOGGER.info("Ran Death Mixin");
         if (damageSource.getSource() instanceof PlayerEntity player) {
-            player.sendMessage(Text.literal("Hello World!"));
-            if (player.getComponent(CardinalComponentRegister.RESURRECTION).getUses() > 0 && player.getComponent(TrinketsApi.TRINKET_COMPONENT).isEquipped(ArtifactRegister.RESURRECT_ARTIFACT)) {
-                player.heal(player.getMaxHealth());
-                player.getComponent(CardinalComponentRegister.RESURRECTION).decrementUses();
-                ci.cancel();
+            TrinketComponent trinkets = player.getComponent(TrinketsApi.TRINKET_COMPONENT);
+            List<Pair<SlotReference, ItemStack>> trinketsEquipped = trinkets.getEquipped(ArtifactRegister.RESURRECT_ARTIFACT);
+            for (Pair<SlotReference, ItemStack> trinket: trinketsEquipped) {
+                ItemStack stack = trinket.getRight();
+                if (stack.getItem() instanceof ArtifactOfResurrectionItem && stack.getNbt().getInt("uses") > 0) {
+                    player.heal(player.getMaxHealth());
+                    ci.cancel();
+                }
             }
         }
     }
