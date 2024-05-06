5416ab080c5154f33835b0d1d9c4efcbdcbc068c
oliver <oliver.joseph.johnson@gmail.com>
2024-05-05 21:54:50 -0400

    PlayerDeathMixin now is being injected by still not running

--- src/main/java/io/oliverj/areas/mixin/PlayerDeathMixin.java
+++ src/main/java/io/oliverj/areas/mixin/PlayerDeathMixin.java
@@ -4,22 +4,29 @@ import dev.emi.trinkets.api.TrinketsApi;
 import io.oliverj.areas.Areas;
 import io.oliverj.areas.registry.ArtifactRegister;
 import io.oliverj.areas.registry.CardinalComponentRegister;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
 public class PlayerDeathMixin {
-    @Inject(at = @At(value = "HEAD"), method = "onDeath", cancellable = true)
+    @Inject(method = "onDeath", at = @At(value = "HEAD"), cancellable = true)
     private void onDeath(DamageSource damageSource, CallbackInfo ci) {
-        PlayerEntity player = (PlayerEntity) damageSource.getSource();
-        if (player.getComponent(CardinalComponentRegister.RESURRECTION).getUses() > 0 && player.getComponent(TrinketsApi.TRINKET_COMPONENT).isEquipped(ArtifactRegister.RESURRECT_ARTIFACT)) {
-            player.heal(player.getMaxHealth());
-            player.getComponent(CardinalComponentRegister.RESURRECTION).decrementUses();
+        ci.cancel();
+        Areas.LOGGER.info("Ran Death Mixin");
+        if (damageSource.getSource() instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) damageSource.getSource();
+            player.sendMessage(Text.literal("Hello World!"));
+            if (player.getComponent(CardinalComponentRegister.RESURRECTION).getUses() > 0 && player.getComponent(TrinketsApi.TRINKET_COMPONENT).isEquipped(ArtifactRegister.RESURRECT_ARTIFACT)) {
+                player.heal(player.getMaxHealth());
+                player.getComponent(CardinalComponentRegister.RESURRECTION).decrementUses();
+                ci.cancel();
+            }
         }
-        Areas.LOGGER.debug("MIXIN RAN");
     }
 }
--- src/main/java/io/oliverj/areas/mixin/TitleScreenMixin.java
+++ src/main/java/io/oliverj/areas/mixin/TitleScreenMixin.java
@@ -2,32 +2,16 @@ package io.oliverj.areas.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.widget.PressableTextWidget;
-import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin extends Screen{
-
-    @Shadow @Final public static Text COPYRIGHT;
-
-    protected TitleScreenMixin(Text title) {
-        super(title);
-    }
-
-    @Inject(method = "render", at = @At("TAIL"))
+public class TitleScreenMixin {
+    @Inject(method = "render", at = @At("HEAD"))
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         Text HOV = Text.literal("Modified by Areas");
         context.drawText(MinecraftClient.getInstance().textRenderer, HOV, 10, 10, 0xffffff, false);
