2743d14c87f5cd2bd92067847d286eaab5d70bec
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-05-07 16:57:03 -0400

    Removed Unnecessary code.

--- src/main/java/io/oliverj/areas/config/AreasConfigModel.java
+++ src/main/java/io/oliverj/areas/config/AreasConfigModel.java
@@ -1,7 +1,10 @@
 package io.oliverj.areas.config;
 
+import io.oliverj.areas.Areas;
 import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Modmenu;
 
+@Modmenu(modId = Areas.MOD_ID)
 @Config(name = "areas-config", wrapperName = "AreasConfig")
 public class AreasConfigModel {
 }
